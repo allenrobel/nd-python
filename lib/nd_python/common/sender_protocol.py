b2748af72c0434880adce9624f3fd967e96b22cb
Allen Robel <arobel@cisco.com>
2025-09-29 11:22:51 -1000

    Appease linters
    - Disable unnecessary-ellipsis as ellipsis is request in Protocol definitions.
    - Add blank line at end of file.

--- lib/nd_python/common/sender_protocol.py
+++ lib/nd_python/common/sender_protocol.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pylint: disable=unnecessary-ellipsis
+
 from typing import Protocol
 
 
